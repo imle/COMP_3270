49ff5f30f5a07deb68e4d9e7a52ae4b8e0c9e06b
Steven Imle (MacBook) <stevenimle@gmail.com>
2016-11-15 03:47:50 -0600

    Implemented Strat3 with new method.

--- src/strat_3/Strat3.cpp
+++ src/strat_3/Strat3.cpp
@@ -11,63 +11,58 @@ int Strat3::run(std::vector<int> values, size_t &index_l, size_t &index_r) {
 		printErrorAndExit(INVALID_VALUE_ARRAY);
 	}
 
+	bool has_negative = false;
+
+	int smallest = values[0];
 	size_t ind_l = 0, ind_r = values.size() - 1;
 
-	int smallest = this->recursive_call(values.begin(), values.end(), ind_l, ind_r);
+	if (values.size() != 1) {
+		for (size_t i = 0; i < values.size(); i++) {
+			if (values[i] < 0) {
+				has_negative = true;
+				break;
+			}
 
-	ind_l -= 1;
-	ind_r -= 1;
+			if (values[i] < smallest) {
+				smallest = values[i];
+				ind_l = i;
+				ind_r = i;
+			}
+		}
 
-	index_l = ind_l;
-	index_r = ind_r;
+		if (has_negative) {
+			int smallest_ending_here = 0;
+			size_t ind_l_ending_here = 0;
+
+			for (size_t i = 0; i < values.size(); i++) {
+				smallest_ending_here += values[i];
+
+				if (smallest_ending_here > 0) {
+					smallest_ending_here = 0;
+					ind_l_ending_here = i + 1;
+				}
+
+				if (smallest > smallest_ending_here) {
+					smallest = smallest_ending_here;
+					ind_l = ind_l_ending_here;
+					ind_r = i;
+				}
+
+				if (smallest > values[i]) {
+					smallest = values[i];
+					ind_l = i;
+					ind_r = i;
+				}
+			}
+		}
+	}
 
 	this->smallest = smallest;
 	this->range.first = ind_l;
 	this->range.second = ind_r;
 
-	return smallest;
-}
-
-int Strat3::recursive_call(vec_iter left, vec_iter right, size_t &index_l, size_t &index_r) {
-	int smallest, current = 0;
-	size_t ind_l = 0, ind_r = (size_t)(right - left - 1);
-
-	if (right - left == 1) {
-		smallest = *left;
-		index_l = ind_l + 1;
-		index_r = ind_r + 1;
-
-		return smallest;
-	}
-
-	for (vec_iter cur = left; cur != right; ++cur) {
-		current += *cur;
-	}
-
-	int top_smallest;
-	size_t top_ind_l, top_ind_r;
-
-	int bot_smallest;
-	size_t bot_ind_l, bot_ind_r;
-
-	top_smallest = this->recursive_call(left, right - 1, top_ind_l, top_ind_r);
-	bot_smallest = this->recursive_call(left + 1, right, bot_ind_l, bot_ind_r);
-
-	if (current < top_smallest && current < bot_smallest) {
-		smallest = current;
-		index_l = ind_l;
-		index_r = ind_r;
-	}
-	else if (top_smallest < current && top_smallest < bot_smallest) {
-		smallest = top_smallest;
-		index_l = top_ind_l + 1;
-		index_r = top_ind_r + 1;
-	}
-	else {
-		smallest = bot_smallest;
-		index_l = bot_ind_l + 1;
-		index_r = bot_ind_r + 1;
-	}
+	index_l = ind_l;
+	index_r = ind_r;
 
 	return smallest;
 }
